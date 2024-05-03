de8540f1f48ff7249a0fbe1f53541f8523d8a43d
Tseringdhoundup9813 <tseringlama9813694977@gmail.com>
2024-05-03 15:03:02 +0545

    create post schema and model

---
+++ server/models/post.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: Object,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    nextEarningDate: {
+      type: Date,
+      default: () =>
+        new Date(new Date().getFullYear, new Date().getMonth() + 1, 1),
+    },
+    thisMonthEarnings: {
+      type: Number,
+      default: 0,
+    },
+    totalEarning: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+    },
+    viewCount: { type: Number, default: 0 },
+    likes: [{ type: mongoose.Schema.Types.ObjectId }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId }],
+    viewers: [{ type: mongoose.Schema.Types.ObjectId }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
+    isBlocked: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const postModel = mongoose.model("post", postSchema);
+
+export default postModel;
